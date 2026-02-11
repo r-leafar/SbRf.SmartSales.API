e3241e56b6042e8a99993b551d4cf209f74adce1
Rafael saboia <r-leafar@hotmail.com>
2026-02-11 18:59:24 -0300

    task: added new validation

--- SbRf.SmartSales.Core/Share/Ensure/EnsureCollection.cs
+++ SbRf.SmartSales.Core/Share/Ensure/EnsureCollection.cs
@@ -21,5 +21,25 @@ namespace SbRf.SmartSales.Core.Share.Ensure
             }
         }
 
+        public static void NoDuplicateValues<T, TKey>( ICollection<T> list,Func<T, TKey> selector,   [CallerArgumentExpression(nameof(list))] string? paramList = null, 
+                [CallerArgumentExpression(nameof(selector))] string? paramName = null)
+        {
+            if (list is null)
+                throw new DomainException($"The list \"{ list }\" is null");
+
+            if (selector is null)
+                throw new DomainException($"The selector \"{selector}\" is nul");
+
+            bool hasDuplicates = list
+                .GroupBy(selector)
+                .Any(g => g.Count() > 1);
+
+            if (hasDuplicates)
+                throw new DomainException(
+                    $"Duplicate values found for {paramName}.");
+
+        }
+
+
     }
 }
